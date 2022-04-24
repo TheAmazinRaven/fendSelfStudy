5fd1e08abe4406b0630b65b6267b83d20ca74b67
EC2 Default User <ec2-user@ip-172-31-1-214.us-east-2.compute.internal>
2022-04-24 19:21:43 +0000

    Testing my commits

---
+++ JavaScript/Udemy/The Complete JavaScript Course/Fundamentals Part 1/Project 1/script.js
@@ -0,0 +1,15 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Raven");
+console.log(28);
+
+let firstName = "Tarrylon";
+let age = 27;
+console.log(firstName);
+console.log(age);
+
+let myFirstJob = "Grocery Store"; // use descriptive variable names.
+let myCurrentJob = "Help Desk";
+
+console.log(test);
