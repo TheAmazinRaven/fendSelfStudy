99e9b0be1fe7b50e7d4c819a6baa61b9559d5852
Raven <raejdickerson@gmail.com>
2022-04-24 23:05:31 -0500

    Worked on variables as well as basic operators.

--- JavaScript/Udemy/The Complete JavaScript Course/Fundamentals Part 1/Project 1/script.js
+++ JavaScript/Udemy/The Complete JavaScript Course/Fundamentals Part 1/Project 1/script.js
@@ -1,5 +1,9 @@
+// Introduction
+
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
+
+// Variables & Assignments
 
 console.log("Raven");
 console.log(28);
@@ -12,4 +16,44 @@ console.log(age);
 let myFirstJob = "Grocery Store"; // use descriptive variable names.
 let myCurrentJob = "Help Desk";
 
-console.log(test);
+console.log("test");
+
+// Data Types
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Raven");
+
+javascriptIsFun = "Yes!";
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1993;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+
+/* Let, const, var
+
+let is for variables that can change later.
+
+const is for variables that are not supposed to change at any point in the future.
+
+var is for the old way of defining variables & should be avoided.
+*/
+
+let age2 = 30;
+age2 = 31; // Variable has been mutated
+
+const birthYear = 1993;
+// birthYear = 1990; - can't do this, it's immutable; cannot declare empty  const variables
+
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Another repo for my self studying adventures but this time in Front End.
 
 4.23.22 Started The Complete JavaScript Course 2022: From Zero to Expert by Jonas Schmedtmann. Set up dev environment, did basic "Hello World!" greeting and learned about values, variables and data types.
+
+4.24.22 Worked on let, const, and var as well as basic operators.
