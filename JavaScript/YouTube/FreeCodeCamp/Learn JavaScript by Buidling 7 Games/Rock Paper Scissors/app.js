e0dbfa1fd98a84b3b49a9cdf9adb220ff7642658
TheAmazinRaven <raejdickerson@gmail.com>
2022-05-17 03:44:05 +0000

    Updated the broken code.

--- JavaScript/YouTube/FreeCodeCamp/Learn JavaScript by Buidling 7 Games/Rock Paper Scissors/app.js
+++ JavaScript/YouTube/FreeCodeCamp/Learn JavaScript by Buidling 7 Games/Rock Paper Scissors/app.js
@@ -1,20 +1,20 @@
 const computerChoiceDisplay = document.getElementById("computer-choice");
 const userChoiceDisplay = document.getElementById("user-choice");
 const resultDisplay = document.getElementById("result");
+const possibleChoices = document.querySelectorAll('button');
 let userChoice;
 let computerChoice;
 let result;
 
-const possibleChoices = document.querySelectorAll("button");
-
-possibleChoices.forEaach(possibleChoices => possibleChoices.addEventListener('click', (e) => {userChoice = e.target.id
-    userChoiceDisplay.innerHTML = userChoice
-    generateComputerChoice()
-    getResult()
-}))
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+  userChoice = e.target.id;
+  userChoiceDisplay.innerHTML = userChoice;
+  generateComputerChoice();
+  getResult();
+}));
 
 function generateComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * 3) +1 
+    const randomNumber = Math.floor(Math.random() * 3) +1; 
     
     if (randomNumber === 1) {
         computerChoice = "rock";
